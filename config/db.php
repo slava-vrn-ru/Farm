6e8f7d4ddfcaffd38f0b8c4505f60ff1162a8635
slava-vrn-ru <mvu_2000@pisem.net>
2017-02-07 14:20:39 +0300

    Changes

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=farm',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Slava',
     'charset' => 'utf8',
 ];
